7bf2156366b8ff2fefb6d11d5e0d50adb0b34d68
Ryan Harrington <Ryanaharrington5@gmail.com>
2019-01-31 14:42:49 -0700

    First Commit

---
+++ main.js
@@ -0,0 +1,23 @@
+function play(game, move) {
+
+}
+
+class Game {
+  constructor(size) {
+    super();
+    this.size = size;
+  }
+
+  makeBoard() {
+    doc
+  }
+}
+
+class Piece {
+  constructor() {
+
+  }
+}
+
+let newGame = new Game(640);
+play()
